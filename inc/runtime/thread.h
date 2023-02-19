14b54f0e3d9bc3b81e615c7c8a27348a3fb5fcbb
Josh Fried <joshuafried@gmail.com>
2023-02-19 00:34:47 -0500

    add space for Thread class in struct thread

--- inc/runtime/thread.h
+++ inc/runtime/thread.h
@@ -69,6 +69,7 @@ struct thread {
      // Trapframe used by junction to stash registers on syscall entry
     struct thread_tf	junction_tf;
     void 		*xsave_area;
+    unsigned long    junction_tstate_buf[8];
 #ifdef GC
     struct list_node    gc_link;
     unsigned int        onk;
