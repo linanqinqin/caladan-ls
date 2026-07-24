yuhong-zhong/caladan

disable stack protector

build/shared.mk
@@ -9,7 +9,7 @@ include $(ROOT_PATH)/build/config
 
 # shared toolchain definitions
 INC = -I$(ROOT_PATH)/inc
-FLAGS  = -g -Wall -D_GNU_SOURCE $(INC) -m64 -mxsavec -m64 -mxsave -m64 -muintr -mfsgsbase
+FLAGS  = -g -Wall -D_GNU_SOURCE $(INC) -m64 -mxsavec -m64 -mxsave -m64 -muintr -mfsgsbase -fno-stack-protector
 LDFLAGS = -T $(ROOT_PATH)/base/base.ld
 LD      = gcc
 CC      = gcc

linanqinqin/caladan-ls

build/shared.mk
@@ -11,7 +11,7 @@ include $(ROOT_PATH)/build/config
 INC = -I$(ROOT_PATH)/inc
 # /* linanqinqin */
 # FLAGS  = -g -Wall -D_GNU_SOURCE $(INC) -m64 -mxsavec -m64 -mxsave -m64
-FLAGS  = -g -Wall -D_GNU_SOURCE $(INC) -m64 -mxsavec -m64 -mxsave -m64 -mxsaveopt -mfsgsbase
+FLAGS  = -g -Wall -D_GNU_SOURCE $(INC) -m64 -mxsavec -m64 -mxsave -m64 -mxsaveopt -mfsgsbase -fno-stack-protector
 # /* end */
 
 ifeq ($(CONFIG_NO_UINTR),n)